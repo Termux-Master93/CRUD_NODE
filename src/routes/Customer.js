1ce48c31ec808cb9366085cd3e9ddd42c578b03c
Termux-Master93 <brisantur8034@gmail.com>
2023-01-13 14:54:53 -0500

    llamar el header.ejs y footer y hacer formulartio dentro

--- src/routes/Customer.js
+++ src/routes/Customer.js
@@ -3,5 +3,7 @@ const router=express.Router();
 
 const custemerController =require('../controllers/customerControler')
 
-router.get('/',custemerController.list)
+router.get('/',custemerController.list);
+router.get('/add',custemerController.save);
+
 module.exports=router;
